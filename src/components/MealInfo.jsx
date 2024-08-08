0ecccb138ea7bb0f294c5d612bc86662594faaa5
bhagyashree-ghude <bhagyashreeghude@gmail.com>
2024-08-08 22:32:54 +0530

    rendeed all the info

--- src/components/MealInfo.jsx
+++ src/components/MealInfo.jsx
@@ -21,18 +21,29 @@ const MealInfo = () => {
   //     getInfo();
   //   }
   return (
-    <div className="grid grid-cols-3 h-full w-full my-20 bg-yellow-500">
-      <img src={info?.strMealThumb} className="w-96 rounded-lg"></img>
-      <div className="col-span-2">
-        <h1 className="text-4xl">Recipe Detail</h1>
-        
+    <div className="bg-yellow-400 bg-cover w-full h-full">
+        {/* <img src="src\components\imgd\view-ready-eat-delicious-meal-go.jpg" alt="bgimg"/> */}
+      {!info ? (
+        "Data Not Found"
+      ) : (
+        <div className="grid grid-cols-3 h-full w-full my-20 bg-yellow-500">
+          <img src={info?.strMealThumb} className="w-96 h-80 rounded-lg"></img>
+          <div className="col-span-2">
+            <h1 className="text-4xl">Recipe Detail</h1>
 
-        <h3 className=" p-4">{info?.strIngredient1}, {info?.strIngredient2}, {info?.strIngredient3}, {info?.strIngredient4}, {info?.strIngredient5}, {info?.strIngredient6}, {info?.strIngredient7}</h3>
-        <button></button>
-        <h3 className="text-2xl">Instruction's</h3>
-        <br></br>
-        <p>{info?.strInstructions}</p>
-      </div>
+            <h3 className=" p-4">
+              {info?.strIngredient1}, {info?.strIngredient2},{" "}
+              {info?.strIngredient3}, {info?.strIngredient4},{" "}
+              {info?.strIngredient5}, {info?.strIngredient6},{" "}
+              {info?.strIngredient7}
+            </h3>
+            <button></button>
+            <h3 className="text-2xl">Instruction's</h3>
+            <br></br>
+            <p>{info?.strInstructions}</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
--- src/components/MealItemInfo.jsx
+++ src/components/MealItemInfo.jsx
@@ -4,7 +4,7 @@ const MealItemInfo = ({detail}) => {
     console.log(detail)
     console.log(detail.idMeal)
   return (
-    <div className="w-96 m-2 p-2 shadow-xl">
+    <div className="w-96 m-2 p-2 shadow-xl ">
         <img  src={detail?.strMealThumb} className="broder border-black rounded-lg shadow-lg"/>
         <p className="font-bold text-2xl">{detail?.strMeal}</p>
         <NavLink to={`/${detail?.idMeal}`}><button className="border border-yellow-500 bg-yellow-500 rounded-lg m-2.5 p-2">Recipe</button></NavLink>
